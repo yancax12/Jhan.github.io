b394430301bd7440493d0ad6218f9064c3a74e53
Jhan Carlos Yangali Huaman <yangaliyanca@gmail.com>
2025-08-31 23:40:09 +0000

    Add deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import github from "@astrojs/github";
 
-// Reemplaza con el nombre de tu repo
+// Nombre de tu repositorio
 const repo = "Jhan.github.io";
 
 export default defineConfig({
   site: `https://tu-usuario.github.io/${repo}`,
-  base: `/${repo}/`, // Importante para GitHub Pages
-  output: "static",
-  adapter: github(),
+  base: `/${repo}/`,
   integrations: [
     mdx(),
     sitemap(),
